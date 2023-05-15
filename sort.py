4ab38f57c700dc1de334e9f95573324593e5319a
Lukas Akdogan <lukas.akdogan@gmail.com>
2023-05-15 14:57:53 +0200

    Update sort.py
    Signed-off-by: Lukas Akdogan <lukas.akdogan@gmail.com>

--- sort.py
+++ sort.py
@@ -1,46 +1,54 @@
-import platform,string
-ip="127.0.0.1 "
-ip2="0.0.0.0 "
-dvb="||"
-s1=""
-s2="^"
-s3="^$important"
-a1=(ip+domain)
-b3=(ip2+domain)
-g6=(dvb+domain)
-input_file="input.txt"
-output_file="output.txt"
-generated_lines=set()
+import platform
+import string
+
+ip = "127.0.0.1 "
+ip2 = "0.0.0.0 "
+dvb = "||"
+s1 = ""
+s2 = "^"
+s3 = "^$important"
+
+a1 = ""
+b3 = ""
+g6 = ""
+
+generated_lines = set()
+
 def generate_suffixes(domain):
-    suffixes=[]
+    suffixes = []
     if not domain.endswith(s2):
-        suffixes.append(a1+s1)
-        suffixes.append(a1+s2)
-        suffixes.append(a1+s3)
-        suffixes.append(b3+s1)
-        suffixes.append(b3+s2)
-        suffixes.append(b3+s3)
-        suffixes.append(g6+s1)
-        suffixes.append(g6+s2)
-        suffixes.append(g6+s3)
+        suffixes.append(a1 + s1)
+        suffixes.append(a1 + s2)
+        suffixes.append(a1 + s3)
+        suffixes.append(b3 + s1)
+        suffixes.append(b3 + s2)
+        suffixes.append(b3 + s3)
+        suffixes.append(g6 + s1)
+        suffixes.append(g6 + s2)
+        suffixes.append(g6 + s3)
     return suffixes
+input_file = "input.txt"
+output_file = "output.txt"
 with open(input_file,"r")as f:
     for line in f:
         line=line.strip()
-        if s2 in line or s3 in line or"^^"in line or"^$importent^"in line:
+        if s2 in line or s3 in line or "^^" in line or "^$important^" in line:
             continue
         if line.startswith(ip):
-            domain=line[len(ip):]
+            domain = line[len(ip):]
+            a1=(ip+domain)
             suffixes=generate_suffixes(domain)
             for suffix in suffixes:
                 if suffix not in generated_lines:
                     generated_lines.add(suffix)
         elif line.startswith(dvb):
             domain=line[len(dvb):]
+            b3=(ip2+domain)
+            g6=(dvb+domain)
             suffixes=generate_suffixes(domain)
             for suffix in suffixes:
                 if suffix not in generated_lines:
                     generated_lines.add(suffix)
-sorted_lines=sorted(generated_lines,key=str.lower)
+sorted_lines=sorted(generated_lines,ey=str.lower)
 with open(output_file,"w")as f:
     f.write("\n".join(sorted_lines))
