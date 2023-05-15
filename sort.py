e1b1942041539861c7bca26c9fa3696a960d4447
Lukas Akdogan <lukas.akdogan@gmail.com>
2023-05-15 17:47:03 +0200

    Update sort.py
    Signed-off-by: Lukas Akdogan <lukas.akdogan@gmail.com>

--- sort.py
+++ sort.py
@@ -5,22 +5,22 @@ dvb="||"
 s1=""
 s2="^"
 s3="^$important"
-a1=""
-b3=""
-g6=""
+p1=""
+p2=""
+p3=""
 generated_lines=set()
 def generate_suffixes(domain):
     suffixes=[]
     if not domain.endswith(s2):
-        suffixes.append(a1+s1)
-        suffixes.append(a1+s2)
-        suffixes.append(a1+s3)
-        suffixes.append(b3+s1)
-        suffixes.append(b3+s2)
-        suffixes.append(b3+s3)
-        suffixes.append(g6+s1)
-        suffixes.append(g6+s2)
-        suffixes.append(g6+s3)
+        suffixes.append(p1+s1)
+        suffixes.append(p1+s2)
+        suffixes.append(p1+s3)
+        suffixes.append(p2+s1)
+        suffixes.append(p2+s2)
+        suffixes.append(p2+s3)
+        suffixes.append(p3+s1)
+        suffixes.append(p3+s2)
+        suffixes.append(p3+s3)
     return suffixes
 input_file="input.txt"
 output_file="output.txt"
@@ -31,19 +31,19 @@ with open(input_file,"r")as f:
             continue
         if line.startswith(ip):
             domain=line[len(ip):]
-            a1=(ip+domain)
+            p1=(ip+domain)
             suffixes=generate_suffixes(domain)
             for suffix in suffixes:
                 if suffix not in generated_lines:
                     generated_lines.add(suffix)
         elif line.startswith(dvb):
             domain=line[len(dvb):]
-            b3=(ip2+domain)
-            g6=(dvb+domain)
+            p2=(ip2+domain)
+            p3=(dvb+domain)
             suffixes=generate_suffixes(domain)
             for suffix in suffixes:
                 if suffix not in generated_lines:
                     generated_lines.add(suffix)
-sorted_lines=sorted(generated_lines,ey=str.lower)
+sorted_lines=sorted(generated_lines,key=str.lower)
 with open(output_file,"w")as f:
     f.write("\n".join(sorted_lines))
