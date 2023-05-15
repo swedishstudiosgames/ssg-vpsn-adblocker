4c67f14cfd982692336dbdfdefa1867ea37d4cc6
Lukas Akdogan <lukas.akdogan@gmail.com>
2023-05-15 15:29:22 +0200

    Update sort.py
    Signed-off-by: Lukas Akdogan <lukas.akdogan@gmail.com>

--- sort.py
+++ sort.py
@@ -1,41 +1,36 @@
-import platform
-import string
-
-ip = "127.0.0.1 "
-ip2 = "0.0.0.0 "
-dvb = "||"
-s1 = ""
-s2 = "^"
-s3 = "^$important"
-
-a1 = ""
-b3 = ""
-g6 = ""
-
-generated_lines = set()
-
+import platform,string
+ip="127.0.0.1 "
+ip2="0.0.0.0 "
+dvb="||"
+s1=""
+s2="^"
+s3="^$important"
+a1=""
+b3=""
+g6=""
+generated_lines=set()
 def generate_suffixes(domain):
-    suffixes = []
+    suffixes=[]
     if not domain.endswith(s2):
-        suffixes.append(a1 + s1)
-        suffixes.append(a1 + s2)
-        suffixes.append(a1 + s3)
-        suffixes.append(b3 + s1)
-        suffixes.append(b3 + s2)
-        suffixes.append(b3 + s3)
-        suffixes.append(g6 + s1)
-        suffixes.append(g6 + s2)
-        suffixes.append(g6 + s3)
+        suffixes.append(a1+s1)
+        suffixes.append(a1+s2)
+        suffixes.append(a1+s3)
+        suffixes.append(b3+s1)
+        suffixes.append(b3+s2)
+        suffixes.append(b3+s3)
+        suffixes.append(g6+s1)
+        suffixes.append(g6+s2)
+        suffixes.append(g6+s3)
     return suffixes
-input_file = "input.txt"
-output_file = "output.txt"
+input_file="input.txt"
+output_file="output.txt"
 with open(input_file,"r")as f:
     for line in f:
         line=line.strip()
-        if s2 in line or s3 in line or "^^" in line or "^$important^" in line:
+        if s2 in line or s3 in line or"^^"in line or"^$important^"in line:
             continue
         if line.startswith(ip):
-            domain = line[len(ip):]
+            domain=line[len(ip):]
             a1=(ip+domain)
             suffixes=generate_suffixes(domain)
             for suffix in suffixes:
